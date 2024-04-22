2300a65a946c87fc550cf4d06d3bee5c5d0543df
Marlon <61248995+bedimcode@users.noreply.github.com>
2024-04-22 08:11:48 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
+}
+
+/* Menu hidden */
+if(navClose){
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
+}
